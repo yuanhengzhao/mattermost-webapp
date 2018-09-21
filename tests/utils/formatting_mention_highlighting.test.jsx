2a0f6253cf6af64e87bd774a4de5b262f81f43d6
Harrison Healey <harrisonmhealey@gmail.com>
2018-09-21 10:42:31 -0400

    Add more unit tests for mention and search highlighting

--- tests/utils/formatting_mention_highlighting.test.jsx
+++ tests/utils/formatting_mention_highlighting.test.jsx
@@ -4,7 +4,7 @@
 import * as TextFormatting from 'utils/text_formatting.jsx';
 
 describe('TextFormatting.mentionHighlighting', () => {
-    const testCases = [{
+    const tests = [{
         name: 'no mentions',
         input: 'These are words',
         mentionKeys: [],
@@ -14,6 +14,11 @@ describe('TextFormatting.mentionHighlighting', () => {
         input: 'These are words',
         mentionKeys: [{key: 'words'}],
         expected: '<p>These are <span class="mention--highlight">words</span></p>',
+    }, {
+        name: 'at-mention without highlight',
+        input: 'This is @user',
+        mentionKeys: [{key: '@words'}],
+        expected: '<p>This is <span data-mention="user">@user</span></p>',
     }, {
         name: 'at-mention',
         input: 'These are @words',
@@ -59,18 +64,58 @@ describe('TextFormatting.mentionHighlighting', () => {
         mentionHighlight: false,
         mentionKeys: [{key: '@words'}],
         expected: '<p>These are @words</p>',
+    }, {
+        name: 'bold',
+        input: 'These are **words** in a sentence',
+        mentionKeys: [{key: 'words'}],
+        expected: '<p>These are <strong><span class="mention--highlight">words</span></strong> in a sentence</p>',
+    }, {
+        name: 'italics',
+        input: 'These _are words in_ a sentence',
+        mentionKeys: [{key: 'words'}],
+        expected: '<p>These <em>are <span class="mention--highlight">words</span> in</em> a sentence</p>',
+    }, {
+        name: 'code span',
+        input: 'These are `words`',
+        mentionKeys: [{key: 'words'}],
+        expected: '<p>These are <span class="codespan__pre-wrap"><code>words</code></span></p>',
+    }, {
+        name: 'code block',
+        input: '```\nThese are\nwords\n```',
+        mentionKeys: [{key: 'words'}],
+        expected:
+            '<div class="post-code post-code--wrap">' +
+                '<code class="hljs">' +
+                    'These are\nwords\n' +
+                '</code>' +
+            '</div>',
+    }, {
+        name: 'link text',
+        input: 'These are [words words](https://example.com)',
+        mentionKeys: [{key: 'words'}],
+        expected: '<p>These are <a class="theme markdown__link" href="https://example.com" rel="noreferrer" target="_blank"><span class="mention--highlight">words</span> <span class="mention--highlight">words</span></a></p>',
+    }, {
+        name: 'link url',
+        input: 'This is [a link](https://example.com/words)',
+        mentionKeys: [{key: 'example'}, {key: 'com'}, {key: 'https'}, {key: 'words'}],
+        expected: '<p>This is <a class="theme markdown__link" href="https://example.com/words" rel="noreferrer" target="_blank">a link</a></p>',
+    }, {
+        name: 'autolinked url',
+        input: 'https://example.com/words',
+        mentionKeys: [{key: 'example'}, {key: 'com'}, {key: 'https'}, {key: 'words'}],
+        expected: '<p><a class="theme markdown__link" href="https://example.com/words" rel="noreferrer" target="_blank">https://example.com/words</a></p>',
     }];
 
-    for (const testCase of testCases) {
-        it(testCase.name, () => {
+    for (const test of tests) {
+        it(test.name, () => {
             const options = {
-                atMentions: 'atMentions' in testCase ? testCase.atMentions : true,
-                mentionHighlight: 'mentionHighlight' in testCase ? testCase.mentionHighlight : true,
-                mentionKeys: testCase.mentionKeys,
+                atMentions: 'atMentions' in test ? test.atMentions : true,
+                mentionHighlight: 'mentionHighlight' in test ? test.mentionHighlight : true,
+                mentionKeys: test.mentionKeys,
             };
-            const output = TextFormatting.formatText(testCase.input, options).trim();
+            const output = TextFormatting.formatText(test.input, options).trim();
 
-            expect(output).toEqual(testCase.expected);
+            expect(output).toEqual(test.expected);
         });
     }
 });
--- tests/utils/formatting_search_highlighting.test.jsx
+++ tests/utils/formatting_search_highlighting.test.jsx
@@ -4,124 +4,173 @@
 import * as TextFormatting from 'utils/text_formatting.jsx';
 
 describe('TextFormatting.searchHighlighting', () => {
-    const testCases = [{
-        name: 'no search highlighting',
-        input: 'These are words in a sentence.',
-        searchMatches: [],
-        searchTerm: '',
-        expected: '<p>These are words in a sentence.</p>',
-    }, {
-        name: 'search term highlighting',
-        input: 'These are words in a sentence.',
-        searchTerm: 'words sentence',
-        expected: '<p>These are <span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span>.</p>',
-    }, {
-        name: 'search term highlighting with quoted phrase',
-        input: 'These are words in a sentence. This is a sentence with words.',
-        searchTerm: '"words in a sentence"',
-        expected: '<p>These are <span class="search-highlight">words in a sentence</span>. This is a sentence with words.</p>',
-    }, {
-        name: 'search term highlighting with empty quoted phrase',
-        input: 'These are words in a sentence. This is a sentence with words.',
-        searchTerm: '""',
-        expected: '<p>These are words in a sentence. This is a sentence with words.</p>',
-    }, {
-        name: 'search term highlighting with flags',
-        input: 'These are words in a sentence.',
-        searchTerm: 'words in:sentence',
-        expected: '<p>These are <span class="search-highlight">words</span> in a sentence.</p>',
-    }, {
-        name: 'search term highlighting with at mentions',
-        input: 'These are @words in a @sentence.',
-        searchTerm: '@words sentence',
-        expected: '<p>These are <span class="search-highlight"><span data-mention="words">@words</span></span> in a <span class="search-highlight"><span data-mention="sentence.">@sentence.</span></span></p>',
-    }, {
-        name: 'search term highlighting in a code span',
-        input: 'These are `words in a sentence`.',
-        searchTerm: 'words',
-        expected: '<p>These are <span class="codespan__pre-wrap"><code><span class="search-highlight">words</span> in a sentence</code></span>.</p>',
-    }, {
-        name: 'search term highlighting in a code block',
-        input: '```\nwords in a sentence\n```',
-        searchTerm: 'words',
-        expected:
-            '<div class="post-code post-code--wrap">' +
-                '<code class="hljs">' +
-                    '<div class="post-code__search-highlighting">' +
-                        '<span class="search-highlight">words</span> in a sentence\n' +
-                    '</div>' +
-                    'words in a sentence\n' +
-                '</code>' +
-            '</div>',
-    }, {
-        name: 'search term highlighting in link text',
-        input: 'These are [words in a sentence](https://example.com).',
-        searchTerm: 'words',
-        expected: '<p>These are <a class="theme markdown__link" href="https://example.com" rel="noreferrer" target="_blank"><span class="search-highlight">words</span> in a sentence</a>.</p>',
-    }, {
-        name: 'search term highlighting in link url',
-        input: 'These are [words in a sentence](https://example.com).',
-        searchTerm: 'example',
-        expected: '<p>These are <a class="theme markdown__link search-highlight" href="https://example.com" rel="noreferrer" target="_blank">words in a sentence</a>.</p>',
-    }, {
-        name: 'search match highlighting',
-        input: 'These are words in a sentence.',
-        searchMatches: ['words', 'sentence'],
-        expected: '<p>These are <span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span>.</p>',
-    }, {
-        name: 'search match highlighting with quoted phrase',
-        input: 'These are words in a sentence. This is a sentence with words.',
-        searchMatches: ['words in a sentence'],
-        expected: '<p>These are <span class="search-highlight">words in a sentence</span>. This is a sentence with words.</p>',
-    }, {
-        name: 'search match highlighting with at mentions',
-        input: 'These are @words in a @sentence.',
-        searchMatches: ['@words', 'sentence'],
-        expected: '<p>These are <span class="search-highlight"><span data-mention="words">@words</span></span> in a <span class="search-highlight"><span data-mention="sentence.">@sentence.</span></span></p>',
-    }, {
-        name: 'search match highlighting in a code span',
-        input: 'These are `words in a sentence`.',
-        searchMatches: ['words'],
-        expected: '<p>These are <span class="codespan__pre-wrap"><code><span class="search-highlight">words</span> in a sentence</code></span>.</p>',
-    }, {
-        name: 'search match highlighting in a code block',
-        input: '```\nwords in a sentence\n```',
-        searchMatches: ['words'],
-        expected:
-            '<div class="post-code post-code--wrap">' +
-                '<code class="hljs">' +
-                    '<div class="post-code__search-highlighting">' +
-                        '<span class="search-highlight">words</span> in a sentence\n' +
-                    '</div>' +
-                    'words in a sentence\n' +
-                '</code>' +
-            '</div>',
-    }, {
-        name: 'search match highlighting in link text',
-        input: 'These are [words in a sentence](https://example.com).',
-        searchMatches: ['words'],
-        expected: '<p>These are <a class="theme markdown__link" href="https://example.com" rel="noreferrer" target="_blank"><span class="search-highlight">words</span> in a sentence</a>.</p>',
-    }, {
-        name: 'search match highlighting in link url',
-        input: 'These are [words in a sentence](https://example.com).',
-        searchMatches: ['example'],
-        expected: '<p>These are <a class="theme markdown__link search-highlight" href="https://example.com" rel="noreferrer" target="_blank">words in a sentence</a>.</p>',
-    }];
+    describe('with client performing highlighting (using searchTerm)', () => {
+        const tests = [{
+            name: 'no highlighting',
+            input: 'These are words in a sentence.',
+            searchTerm: '',
+            expected: '<p>These are words in a sentence.</p>',
+        }, {
+            name: 'regular words',
+            input: 'These are words in a sentence.',
+            searchTerm: 'words sentence',
+            expected: '<p>These are <span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span>.</p>',
+        }, {
+            name: 'quoted phrase',
+            input: 'These are words in a sentence. This is a sentence with words.',
+            searchTerm: '"words in a sentence"',
+            expected: '<p>These are <span class="search-highlight">words in a sentence</span>. This is a sentence with words.</p>',
+        }, {
+            name: 'empty quoted phrase',
+            input: 'These are words in a sentence. This is a sentence with words.',
+            searchTerm: '""',
+            expected: '<p>These are words in a sentence. This is a sentence with words.</p>',
+        }, {
+            name: 'search flags',
+            input: 'These are words in a sentence.',
+            searchTerm: 'words in:sentence',
+            expected: '<p>These are <span class="search-highlight">words</span> in a sentence.</p>',
+        }, {
+            name: 'at mentions',
+            input: 'These are @words in a @sentence.',
+            searchTerm: '@words sentence',
+            expected: '<p>These are <span class="search-highlight"><span data-mention="words">@words</span></span> in a <span class="search-highlight"><span data-mention="sentence.">@sentence.</span></span></p>',
+        }, {
+            name: 'bold',
+            input: 'These are **words in a sentence**.',
+            searchTerm: 'words sentence',
+            expected: '<p>These are <strong><span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span></strong>.</p>',
+        }, {
+            name: 'italics',
+            input: 'These are _words in a sentence_.',
+            searchTerm: 'words sentence',
+            expected: '<p>These are <em><span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span></em>.</p>',
+        }, {
+            name: 'code span',
+            input: 'These are `words in a sentence`.',
+            searchTerm: 'words',
+            expected: '<p>These are <span class="codespan__pre-wrap"><code><span class="search-highlight">words</span> in a sentence</code></span>.</p>',
+        }, {
+            name: 'code block',
+            input: '```\nwords in a sentence\n```',
+            searchTerm: 'words',
+            expected:
+                '<div class="post-code post-code--wrap">' +
+                    '<code class="hljs">' +
+                        '<div class="post-code__search-highlighting">' +
+                            '<span class="search-highlight">words</span> in a sentence\n' +
+                        '</div>' +
+                        'words in a sentence\n' +
+                    '</code>' +
+                '</div>',
+        }, {
+            name: 'link text',
+            input: 'These are [words in a sentence](https://example.com).',
+            searchTerm: 'words',
+            expected: '<p>These are <a class="theme markdown__link" href="https://example.com" rel="noreferrer" target="_blank"><span class="search-highlight">words</span> in a sentence</a>.</p>',
+        }, {
+            name: 'link url',
+            input: 'These are [words in a sentence](https://example.com).',
+            searchTerm: 'example',
+            expected: '<p>These are <a class="theme markdown__link search-highlight" href="https://example.com" rel="noreferrer" target="_blank">words in a sentence</a>.</p>',
+        }, {
+            name: 'autolinked url',
+            input: 'https://example.com/words',
+            searchTerm: 'example words',
+            expected: '<p><a class="theme markdown__link search-highlight" href="https://example.com/words" rel="noreferrer" target="_blank">https://example.com/words</a></p>',
+        }];
 
-    for (const testCase of testCases) {
-        it(testCase.name, () => {
-            const options = {
-                atMentions: 'atMentions' in testCase ? testCase.atMentions : true,
-                mentionHighlight: 'mentionHighlight' in testCase ? testCase.mentionHighlight : true,
-                mentionKeys: testCase.mentionKeys,
-                searchMatches: testCase.searchMatches,
-                searchTerm: testCase.searchTerm || '',
-            };
-            const output = TextFormatting.formatText(testCase.input, options).trim();
+        for (const test of tests) {
+            it(test.name, () => {
+                const options = {
+                    atMentions: true,
+                    searchTerm: test.searchTerm,
+                };
+                const output = TextFormatting.formatText(test.input, options).trim();
 
-            expect(output).toEqual(testCase.expected);
-        });
-    }
+                expect(output).toEqual(test.expected);
+            });
+        }
+    });
+
+    describe('with server performing highlighting (using searchMatches)', () => {
+        const tests = [{
+            name: 'no highlighting',
+            input: 'These are words in a sentence.',
+            searchMatches: [],
+            expected: '<p>These are words in a sentence.</p>',
+        }, {
+            name: 'regular words',
+            input: 'These are words in a sentence.',
+            searchMatches: ['words', 'sentence'],
+            expected: '<p>These are <span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span>.</p>',
+        }, {
+            name: ' quoted phrase',
+            input: 'These are words in a sentence. This is a sentence with words.',
+            searchMatches: ['words in a sentence'],
+            expected: '<p>These are <span class="search-highlight">words in a sentence</span>. This is a sentence with words.</p>',
+        }, {
+            name: 'at mentions',
+            input: 'These are @words in a @sentence.',
+            searchMatches: ['@words', 'sentence'],
+            expected: '<p>These are <span class="search-highlight"><span data-mention="words">@words</span></span> in a <span class="search-highlight"><span data-mention="sentence.">@sentence.</span></span></p>',
+        }, {
+            name: 'bold',
+            input: 'These are **words in a sentence**.',
+            searchMatches: ['words', 'sentence'],
+            expected: '<p>These are <strong><span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span></strong>.</p>',
+        }, {
+            name: 'italics',
+            input: 'These are _words in a sentence_.',
+            searchMatches: ['words', 'sentence'],
+            expected: '<p>These are <em><span class="search-highlight">words</span> in a <span class="search-highlight">sentence</span></em>.</p>',
+        }, {
+            name: 'code span',
+            input: 'These are `words in a sentence`.',
+            searchMatches: ['words'],
+            expected: '<p>These are <span class="codespan__pre-wrap"><code><span class="search-highlight">words</span> in a sentence</code></span>.</p>',
+        }, {
+            name: 'code block',
+            input: '```\nwords in a sentence\n```',
+            searchMatches: ['words'],
+            expected:
+                '<div class="post-code post-code--wrap">' +
+                    '<code class="hljs">' +
+                        '<div class="post-code__search-highlighting">' +
+                            '<span class="search-highlight">words</span> in a sentence\n' +
+                        '</div>' +
+                        'words in a sentence\n' +
+                    '</code>' +
+                '</div>',
+        }, {
+            name: 'link text',
+            input: 'These are [words in a sentence](https://example.com).',
+            searchMatches: ['words'],
+            expected: '<p>These are <a class="theme markdown__link" href="https://example.com" rel="noreferrer" target="_blank"><span class="search-highlight">words</span> in a sentence</a>.</p>',
+        }, {
+            name: 'link url',
+            input: 'These are [words in a sentence](https://example.com).',
+            searchMatches: ['example'],
+            expected: '<p>These are <a class="theme markdown__link search-highlight" href="https://example.com" rel="noreferrer" target="_blank">words in a sentence</a>.</p>',
+        }, {
+            name: 'autolinked url',
+            input: 'https://example.com/words',
+            searchMatches: ['example', 'words'],
+            expected: '<p><a class="theme markdown__link search-highlight" href="https://example.com/words" rel="noreferrer" target="_blank">https://example.com/words</a></p>',
+        }];
+
+        for (const test of tests) {
+            it(test.name, () => {
+                const options = {
+                    atMentions: true,
+                    searchMatches: test.searchMatches,
+                };
+                const output = TextFormatting.formatText(test.input, options).trim();
+
+                expect(output).toEqual(test.expected);
+            });
+        }
+    });
 
     it('wildcard highlighting', () => {
         assertTextMatch('foobar', 'foo*', 'foo', 'bar');
